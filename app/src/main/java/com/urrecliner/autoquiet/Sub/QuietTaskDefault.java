aa9757f4971a0abcd0b6b32caae36329ea4773a6
Woncherl Ha (하원철) <riopapa@gmail.com>
2023-01-29 11:46:36 +0900

    All restructured with bug fix

--- app/src/main/java/com/urrecliner/autoquiet/Sub/QuietTaskDefault.java
+++ app/src/main/java/com/urrecliner/autoquiet/Sub/QuietTaskDefault.java
@@ -14,8 +14,9 @@ public class QuietTaskDefault {
         int mStart = calendar.get(Calendar.MINUTE);
         calendar.add(Calendar.HOUR_OF_DAY, 1);
         int hFinish = calendar.get(Calendar.HOUR_OF_DAY);
-        boolean [] week = new boolean[]{false, true, true, true, true, true, false};
+        boolean [] week = new boolean[7];
+        week[calendar.get(Calendar.DAY_OF_WEEK) - 1] = true;
         return new QuietTask("추가할 제목", hStart, mStart, hFinish, mStart,
-                week, true, true, 1, 0);
+                week, true, true, 1, 1);
     }
 }
