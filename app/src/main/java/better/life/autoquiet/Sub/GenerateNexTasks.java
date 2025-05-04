0c8d32202b7a1d795ccef087500104ffa64bc9ab
Woncherl Ha (하원철) <riopapa@gmail.com>
2025-05-04 17:29:11 +0900

    enhance schedule next

--- app/src/main/java/better/life/autoquiet/Sub/GenerateNexTasks.java
+++ app/src/main/java/better/life/autoquiet/Sub/GenerateNexTasks.java
@@ -3,11 +3,10 @@ package better.life.autoquiet.Sub;
 import static better.life.autoquiet.activity.ActivityAddEdit.BELL_SEVERAL;
 import static better.life.autoquiet.activity.ActivityMain.nextTasks;
 
-import android.util.Log;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Locale;
 
 import better.life.autoquiet.models.NextTask;
@@ -17,14 +16,13 @@ public class GenerateNexTasks {
 
     QuietTask qt;
     int several;
+    ArrayList <NextTask> nTs;
+    final SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd, HH:mm", Locale.getDefault());
 
     public void gen(ArrayList<QuietTask> quietTasks) {
-        ArrayList <NextTask> nTs;
         nTs = new ArrayList<>();
-        final SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd, HH:mm", Locale.getDefault());
         final long nowTime = System.currentTimeMillis()+ 30000;
-        final long farTime = nowTime + 36*60*60*1000;
-        long nxtStart, nxtFinish;
+        final long farTime = nowTime + 30*60*60*1000;
 
         for (int idx = 0; idx < quietTasks.size(); idx++) {
             qt = quietTasks.get(idx);
@@ -35,90 +33,70 @@ public class GenerateNexTasks {
             cal.set(Calendar.SECOND, 0);
             cal.set(Calendar.MILLISECOND, 0);
             int WKStart = cal.get(Calendar.DAY_OF_WEEK) - 1; // 1 for sunday
-            for (int wkNbr = WKStart; ; ) {
-                if (qt.week[wkNbr]) {
-                    cal.set(Calendar.HOUR_OF_DAY, qt.begHour);
-                    cal.set(Calendar.MINUTE, qt.begMin);
-                    nxtStart = cal.getTimeInMillis();
-                    if (qt.endHour == 99)
-                        nxtFinish = 0;
-                    else {
+            int WKFinish = WKStart + 3;
+            boolean[] twoWeeks = new boolean[14];
+            System.arraycopy(qt.week, 0, twoWeeks, 0, 7);
+            System.arraycopy(qt.week, 0, twoWeeks, 7, 7);
+            cal.set(Calendar.HOUR_OF_DAY, qt.begHour);
+            cal.set(Calendar.MINUTE, qt.begMin);
+
+            for (int wkNbr = WKStart; wkNbr < WKFinish; wkNbr++) {
+                if (twoWeeks[wkNbr]) {
+                    add2NextTasks(idx,"S", cal, qt);
+                    if (qt.endHour != 99) {
                         Calendar cal2 = (Calendar) cal.clone();
                         cal2.set(Calendar.HOUR_OF_DAY, qt.endHour);
                         cal2.set(Calendar.MINUTE, qt.endMin);
                         if (qt.begHour > qt.endHour) {
                             cal2.add(Calendar.DATE, 1);
                         }
-                        nxtFinish = cal2.getTimeInMillis();
+                        add2NextTasks(idx,"F", cal2, qt);
                     }
-                    if (nowTime < nxtStart || nowTime < nxtFinish)
-                        break;
                 }
-                wkNbr++;
-                cal.add(Calendar.DAY_OF_MONTH, 1);
-                if (wkNbr == 7)
-                    wkNbr = 0;
-            }
-
-            if (nowTime < nxtStart) {
-                if (qt.alarmType == BELL_SEVERAL) {
-                    nxtStart += 12000;
-                    several = 2;
-                } else
-                    several = 0;
-                NextTask nt = new NextTask();
-                nt.time = nxtStart;
-                nt.timeS = sdf.format(nxtStart);
-                nt.SFO = "S";
-                nt.subject = qt.subject;
-                nt.several = several;
-                nt.alarmType = qt.alarmType;
-                nt.idx = idx;
-                nt.hour = qt.begHour;
-                nt.min = qt.begMin;
-                nt.suffix = (qt.endHour == 99)? "" : "시작";
-                nt.vibrate = qt.vibrate;
-                nt.sayDate = qt.sayDate;
-                nt.clock = qt.clock;
-                nTs.add(nt);
-            }
-            if (qt.endHour != 99 && nowTime < nxtFinish) {
-                several = (qt.sayDate) ? 3 : 0;
-                NextTask nt = new NextTask();
-                nt.time = nxtFinish;
-                nt.timeS = sdf.format(nxtFinish);
-                nt.SFO = "F";
-                nt.subject = qt.subject;
-                nt.several = several;
-                nt.alarmType = qt.alarmType;
-                nt.idx = idx;
-                nt.hour = qt.endHour;
-                nt.min = qt.endMin;
-                nt.suffix = "까지";
-                nt.vibrate = qt.vibrate;
-                nt.sayDate = qt.sayDate;
-                nTs.add(nt);
+                cal.add(Calendar.DATE, 1);
             }
         }
-
-//        for (NextTask nt: nTs) {
-//            String [] ss = {nt.timeS, ""+nt.hour, ""+nt.min, nt.subject, ""+ nt.alarmType, nt.suffix, ""+ nt.idx};
-//            Log.w("nTs "+nt.idx, String.join(", ", ss));
-//        }
 
         nextTasks = new ArrayList<>();
         for (NextTask nt: nTs) {
             if (nt.time > nowTime && nt.time < farTime)
                 nextTasks.add(nt);
         }
-        nextTasks.sort((nt1, nt2) -> Long.compare(nt1.time, nt2.time));
-        Log.w("nxt ", "---");
-//
-//        for (NextTask nt: nextTasks) {
-//            String [] ss = {nt.timeS, ""+nt.hour, ""+nt.min, nt.subject, ""+ nt.alarmType, nt.suffix, ""+ nt.idx};
-//            Log.w("nxt "+nt.idx, String.join(", ", ss));
-//        }
+        nextTasks.sort(Comparator.comparingLong(nt -> nt.time));
+    }
 
+    private void add2NextTasks(int idx, String SFO, Calendar cal, QuietTask qt) {
+        long setTime = cal.getTimeInMillis();
+        if (qt.alarmType == BELL_SEVERAL) {
+            setTime += 12000;
+            several = 2;
+        } else
+            several = 0;
+        NextTask nt = new NextTask();
+        nt.time = setTime;
+        nt.timeS = sdf.format(setTime);
+        nt.SFO = SFO;
+        nt.subject = qt.subject;
+        nt.several = several;
+        nt.alarmType = qt.alarmType;
+        nt.idx = idx;
+        if (SFO.equals("F")) {
+            nt.hour = qt.endHour;
+            nt.min = qt.endMin;
+            if (qt.endHour != 99)
+                nt.suffix = "까지";
+        } else {
+            nt.hour = qt.begHour;
+            nt.min = qt.begMin;
+            if (qt.endHour != 99)
+                nt.suffix = "시작";
+            else
+                nt.suffix = "";
+        }
+        nt.vibrate = qt.vibrate;
+        nt.sayDate = qt.sayDate;
+        nt.clock = qt.clock;
+        nTs.add(nt);
     }
 
 }
